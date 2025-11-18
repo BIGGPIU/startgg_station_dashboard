e972f7f69d9eb431fd584f3c6ff4ec7f6a377797
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2025-11-18 12:36:42 -0500

    update fr

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "https://biggpiu.github.io/"
+  base: "https://biggpiu.github.io/startgg_station_dashboard/"
 })
